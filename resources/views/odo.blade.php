1e8e844fcfa1d3af3ae5e726ee5c3e800f365169
Christian Busch <kontakt@christian-busch.com>
2022-02-04 18:52:18 +0100

    text changes

--- resources/views/odo.blade.php
+++ resources/views/odo.blade.php
@@ -43,7 +43,7 @@
         </div>
         <div class="row">
            <div class="col-12">
-                Zuletzt aktualisiert: {{$last_updated}} GMT
+                Letzte Fahrt: {{$last_updated}} GMT
            </div>
 
        </div>
